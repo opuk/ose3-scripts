490f4222ef176a9a919c5811cd914bb199f1d5c8
jswensso <jswensso@redhat.com>
2015-08-05 17:39:39 +0100

    initial commit

---
+++ prephost.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+subscription-manager repos --disable="*"
+subscription-manager repos \
+    --enable="rhel-7-server-rpms" \
+    --enable="rhel-7-server-extras-rpms" \
+    --enable="rhel-7-server-optional-rpms" \
+    --enable="rhel-7-server-ose-3.0-rpms"
+yum remove NetworkManager -y
+yum install wget git net-tools bind-utils iptables-services bridge-utils -y
+#quick install only?
+yum install gcc python-virtualenv -y
+yum update -y
+yum install docker -y
+sed -i "s/OPTIONS.*/OPTIONS='--selinux-enabled --insecure-registry 172.30.0.0\/0'/" /etc/sysconfig/docker
+
