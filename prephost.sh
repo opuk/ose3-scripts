b144d09bdce34ca607f9e09ba6364950ba50bd6c
Johan Swensson <johan@redhat.com>
2015-12-13 13:56:13 +0100

    add satellite tools

--- prephost.sh
+++ prephost.sh
@@ -5,6 +5,7 @@ subscription-manager repos \
     --enable="rhel-7-server-rpms" \
     --enable="rhel-7-server-extras-rpms" \
     --enable="rhel-7-server-optional-rpms" \
+    --enable="rhel-7-server-satellite-tools-6.1-rpms" \
     --enable="rhel-7-server-ose-3.1-rpms"
 yum remove NetworkManager -y
 yum install wget git net-tools bind-utils iptables-services bridge-utils -y
