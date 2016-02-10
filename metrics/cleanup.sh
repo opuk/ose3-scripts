f274fc0458b23eff1cd3648bf924d428a6f7baab
Johan Swensson <johan@redhat.com>
2016-02-10 21:39:37 +0100

    add metrics cleanup script

---
+++ metrics/cleanup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for i in $(oc get secret | egrep "(hawkular|heapster|metrics)" | awk '{ print $1 }'); do
+  oc delete secret $i
+done
+
+oc delete rc hawkular-metrics heapster hawkular-cassandra-1
+oc delete svc hawkular-cassandra hawkular-cassandra-nodes hawkular-metrics heapster 
+oc delete route hawkular-metrics
+oc delete sa cassandra hawkular heapster
+oc delete template hawkular-cassandra-node-emptydir hawkular-cassandra-node-pv hawkular-cassandra-services hawkular-heapster hawkular-metrics hawkular-support
+oc delete pvc metrics-cassandra-1
