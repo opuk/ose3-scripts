726bb53bcef7d6d12b4bd684e91dd0a9b6aae198
Johan Swensson <johan@redhat.com>
2016-03-03 23:54:10 +0100

    add populate-node.sh

---
+++ populate-node.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker pull registry.access.redhat.com/openshift3/ose-haproxy-router
+docker pull registry.access.redhat.com/openshift3/ose-deployer
+docker pull registry.access.redhat.com/openshift3/ose-sti-builder
+docker pull registry.access.redhat.com/openshift3/ose-docker-builder
+docker pull registry.access.redhat.com/openshift3/ose-pod
+docker pull registry.access.redhat.com/openshift3/ose-docker-registry
+
+
