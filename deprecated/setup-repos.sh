e18ef70e7fe8581c18356b29fae6b63ad78779b5
Johan Swensson <johan@redhat.com>
2016-10-08 22:19:17 +0200

    drop old stuff

--- deprecated/setup-repos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#before running this script, register and attach subscription
-subscription-manager repos --disable="*"
-subscription-manager repos \
-    --enable="rhel-7-server-rpms" \
-    --enable="rhel-7-server-extras-rpms" \
-    --enable="rhel-7-server-optional-rpms" \
-    --enable="rhel-7-server-satellite-tools-6.1-rpms" \
-    --enable="rhel-7-server-ose-3.1-rpms"
