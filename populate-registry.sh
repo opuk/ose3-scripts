a14e6caace884d1d873932ace8b91f915261a9df
Johan Swensson <johan@redhat.com>
2018-01-31 21:23:41 +0100

    update registry sync script

--- populate-registry.sh
+++ populate-registry.sh
@@ -1,15 +1,14 @@
-#!/bin/bash
-
-if ! [ -x "/usr/bin/skopeo" ]; then
-  echo 'Error: skopeo is not installed. It is available in rhel-7-server-extras-rpms.'
-  exit 1
-fi
+#!/bin/sh
 
 OSE_VERS=3.7
-UPSTREAM_REGISTRY=registry.access.redhat.com
-REGISTRY=registry.lab.kupo.se:5000
+REG_VERS=2
+UPSTREAM=registry.access.redhat.com
+REGISTRY=registry.example.com:5000
+# https://bugzilla.redhat.com/show_bug.cgi?id=1481130
+USE_SKOPEO=no
+SKOPEO_DEST_VERIFY=false
 
-DEST_REGISTRY_SECURE=true
+# Add/remove XXX to variable names to disable/enable syncing of the images
 
 ose_images="
   openshift3/ose-deployer
@@ -24,6 +23,7 @@ ose_images="
   openshift3/logging-elasticsearch
   openshift3/logging-fluentd
   openshift3/logging-kibana
+  openshift3/oauth-proxy
   openshift3/metrics-cassandra
   openshift3/metrics-hawkular-metrics
   openshift3/metrics-heapster
@@ -57,7 +57,7 @@ xpaas_images="
   jboss-eap-7/eap70-openshift
   redhat-sso-7/sso70-openshift
   rhscl/postgresql-95-rhel7
-  rhscl/nodejs-4-rhel7
+  rhscl/nodejs-6-rhel7
 "
 
 jenkins_images="
@@ -74,11 +74,35 @@ gluster_images="
   rhgs3/rhgs-s3-server-rhel7
 "
 
-# Pull
-for img in $ose_images $ose_images_cont; do
-  avail="$(curl -s https://$UPSTREAM_REGISTRY/v1/repositories/$img/tags | grep -Po '"v?'${OSE_VERS/\./\\.}'.*?"' | tr -d '"' | sort -V)"
+# Configure Docker if needed
+[ "$USE_SKOPEO" != "yes" ] && (rpm -q docker > /dev/null 2>&1 || yum install -y docker)
+if [ "$USE_SKOPEO" != "yes" ] && ! grep -q "add-registry $REGISTRY" /etc/sysconfig/docker; then
+  systemctl stop docker
+  sed -i -e 's,--log-driver=,--log-level=warn --log-driver=,' /etc/sysconfig/docker
+  sed -i -e 's,--log-level=,--max-concurrent-downloads=10 --log-level=,' /etc/sysconfig/docker
+  sed -i -e 's,--log-level=,--max-concurrent-uploads=10 --log-level=,' /etc/sysconfig/docker
+  sed -i -e 's,^ADD_REGISTRY=,#ADD_REGISTRY=,' /etc/sysconfig/docker
+  sed -i -e 's,^BLOCKED_REGISTRY=,#BLOCKED_REGISTRY=,' /etc/sysconfig/docker
+  sed -i -e 's,^INSECURE_REGISTRY=,#INSECURE_REGISTRY=,' /etc/sysconfig/docker
+  cat <<EOF>> /etc/sysconfig/docker
+ADD_REGISTRY='--add-registry $REGISTRY --add-registry $UPSTREAM'
+BLOCK_REGISTRY='--block-registry all'
+EOF
+  if [ $REG_VERS -eq 1 ]; then
+    echo INSECURE_REGISTRY=\'--insecure-registry $REGISTRY\' >> /etc/sysconfig/docker
+  fi
+  systemctl enable docker
+fi
+[ "$USE_SKOPEO" != "yes" ] && systemctl start docker
+
+# Pull/copy
+for img in $ose_images $ose_images_cont $ose_images_opt; do
+  avail="$(curl -s https://$UPSTREAM/v1/repositories/$img/tags | grep -Po '"v?'${OSE_VERS/\./\\.}'.*?"' | tr -d '"' | sort -V)"
   # rhel7/etcd has its own versioning
-  [ "$img" = "rhel7/etcd" ] && skopeo copy --dest-tls-verify=$DEST_REGISTRY_SECURE docker://$UPSTREAM_REGISTRY/$img docker://$REGISTRY/$img
+  if [ "$img" = "rhel7/etcd" ]; then
+    [ "$USE_SKOPEO" != "yes" ] && docker pull $UPSTREAM/$img
+    [ "$USE_SKOPEO"  = "yes" ] && echo Copying $img... && skopeo copy --dest-tls-verify=$SKOPEO_DEST_VERIFY --dest-cert-dir=/etc/docker-distribution/registry docker://$UPSTREAM/$img docker://$REGISTRY/$img
+  fi
   [ -n "$avail" ] || continue
   # Get latest images with and without v in the tag / patch level
   tags=""
@@ -88,17 +112,39 @@ for img in $ose_images $ose_images_cont; do
   tags="$tags $(printf %s\\n $avail | grep ^v | grep -v -- - | tail -n 1)"
   tags="$tags $(printf %s\\n $avail | grep -v ^v | grep -v -- - | tail -n 1)"
   tags="$(echo $tags | tr ' ' '\n' | sort -u)"
-  echo -n "Tags to sync: "
-  echo $tags
   for tag in $tags; do
-    echo "Syncing $img:$tag"
-    skopeo copy --dest-tls-verify=$DEST_REGISTRY_SECURE docker://$UPSTREAM_REGISTRY/$img:$tag docker://$REGISTRY/$img:$tag
+    if [ "$USE_SKOPEO" != "yes" ]; then
+      docker pull $UPSTREAM/$img:$tag || exit 1
+    else
+      echo Copying $img:$tag...
+      skopeo copy --dest-tls-verify=$SKOPEO_DEST_VERIFY --dest-cert-dir=/etc/docker-distribution/registry docker://$UPSTREAM/$img:$tag docker://$REGISTRY/$img:$tag || exit 1
+    fi
   done
 done
 
-exit 0
-
 for img in $xpaas_images $jenkins_images $gluster_images; do
   # Latest only
-  skopeo copy --dest-tls-verify=$DEST_REGISTRY_SECURE docker://$UPSTREAM_REGISTRY/$img docker://$REGISTRY/$img
+  if [ "$USE_SKOPEO" != "yes" ]; then
+    docker pull $UPSTREAM/$img || exit 2
+  else
+    echo Copying $img...
+    skopeo copy --dest-tls-verify=$SKOPEO_DEST_VERIFY --dest-cert-dir=/etc/docker-distribution/registry docker://$UPSTREAM/$img docker://$REGISTRY/$img || exit 2
+  fi
 done
+
+# Push
+if [ "$USE_SKOPEO" != "yes" ]; then
+  images="$(docker images)"
+  for img in $ose_images $ose_images_cont $ose_images_opt $xpaas_images $jenkins_images; do
+    for tag in $(printf %s\\n "$images" | awk '/'$UPSTREAM\\/${img/\//\\/}' / {print $2}'); do
+      [ "$tag" = "<none>" ] && continue
+      docker tag $UPSTREAM/$img:$tag $REGISTRY/$img:$tag || exit 3
+      docker push $REGISTRY/$img:$tag || exit 4
+      docker rmi $REGISTRY/$img:$tag || exit 5
+    done
+  done
+fi
+
+# Garbage collect
+/usr/bin/registry garbage-collect /etc/docker-distribution/registry/config.yml
+
